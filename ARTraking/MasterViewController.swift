870f131cdb193dda681f3912c187f1452011847d
hardik-softweb <hardik.gajjar@softwebsolutions.com>
2018-07-04 15:03:16 +0530

    initial

--- ARTraking/MasterViewController.swift
+++ ARTraking/MasterViewController.swift
@@ -12,8 +12,6 @@ class MasterViewController: UITableViewController {
 
     var detailViewController: DetailViewController? = nil
     var objects = [Any]()
-
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
